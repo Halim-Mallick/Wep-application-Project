d862f9a937c57dde8257d035676a6b8990050ced
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-06-23 16:06:38 +0600

    2.1

--- Eid-2023_2/index.php
+++ Eid-2023_2/index.php
@@ -3,21 +3,29 @@
 <head>
     <title>Greeting Message</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"> -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Play&display=swap');
     </style>
 </head>
 <body>
-<div class="header">
-        <button onclick="reloadPage()">Reload</button>
+<!-- <div class="header">
+    <button onclick="reloadPage()">Reload</button>
+</div> -->
+<div class="headline">
+    <h3>আপনার প্রিয়জনকে স্পেশাল ভাবে উইশ করুন</h3>
 </div>
 <div class="container">
         <h1>Birthday Greetings!</h1>
         <p id="Main-message">You can wish your friend by your name.</p>
-        <button onclick="YourName()">Enter Your Name</button>
-        <button onclick="FrndName()">Enter Friend Name</button>
+        <button  onclick="YourName()">Enter Your Name</button>
+        <button id="b2" onclick="FrndName()">Enter Friend Name</button>
 </div>
-
+<div class="footer">
+    <h6>Dev: Mallick</h6>
+</div>
     <script src="script.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> -->
+    
 </body>
 </html>
