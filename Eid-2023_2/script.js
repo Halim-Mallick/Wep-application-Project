7311c6f4b1f9ad9411f600ea91a7808e56644f9a
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-06-23 16:07:05 +0600

    2.1

--- Eid-2023_2/script.js
+++ Eid-2023_2/script.js
@@ -1,24 +1,23 @@
-// script.js
+//Script.js
 function reloadPage(){
   location.reload();
 }
 
 function YourName() {
   var name = prompt("Enter your name:");
-  if (name) {
+  if (name){
     var message = document.getElementById("Main-message");
     message.innerHTML = 'Wishing from <span class="name"><br>'+ name + "</span><br> Many Many Happy Returns of the day! <br><span class=b_day><span id='birthdayText'></span><span>";
-   
-    // Typed text
+
     var birthdayText = document.getElementById("birthdayText");
     var text = "Happy Birthday To You!";
     var typingSpeed = 70; // Adjust the typing speed (in milliseconds)
     var index = 0;
 
-    function typeText() {
+    function typeText(){
       birthdayText.textContent += text[index];
       index++;
-      if (index < text.length) {
+      if (index < text.length){
         setTimeout(typeText, typingSpeed);
       }
     }
@@ -38,6 +37,6 @@ function FrndName() {
 function updateMessage(){
   var message=document.getElementById("Main-message");
   var friendMessage = document.createElement("p");
-  friendMessage.innerHTML = "<span class='fname'> " + friend[friend.length - 1] + "!</span>";
+  friendMessage.innerHTML = "<span class='fname'>❤" + friend[friend.length - 1] + "❤</span>";
   message.parentNode.insertBefore(friendMessage, message.nextSibling)
 }
